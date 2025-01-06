5c7c9e533d91477d630eee4392c1750022a1341c
Davi Fernandes <davi.fer159@gmail.com>
2025-01-06 10:56:25 -0300

    life cycle

--- life-cycle/src/App.jsx
+++ life-cycle/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Fase 1 Componente Montado!");
+  }, []);
+
+  useEffect(() => {
+    if (count !== 0) {
+      console.log("Fase 2 Componente Atualizado");
+    }
+  }, [count]);
+
+  useEffect(() => {
+    if (count < 0) console.log("Fase 3 Componente Desmontado");
+  }, [count]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {console.log("Renderizou")}
+      <h1>Life Cycle React</h1>
+      {count >= 0 && <h2>{count}</h2>}
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <button onClick={() => setCount((c) => c - 1)}>-</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
