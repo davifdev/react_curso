11ad879000f5a3aa3f08612e80374932b89c271c
Davi Fernandes <davi.fer159@gmail.com>
2025-01-13 17:36:35 -0300

    use-context

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,14 +1,22 @@
 import { HookUseEffect } from "./components/HookUseEffect";
 import { HookUseReducer } from "./components/HookUseReducer";
 import { HookUseState } from "./components/HookUseState";
+import { HookUseContextProvider } from "./components/HookUseContext";
 
+import { useState } from "react";
 function App() {
+  const [user, setUser] = useState(false);
+  
   return (
-    <>
+    <HookUseContextProvider value={{ user }}>
       <HookUseState />
       <HookUseReducer />
       <HookUseEffect />
-    </>
+      <hr />
+      <h1>Hook useContext</h1>
+      <h3>Usuário está logado ?</h3>
+      {user ? <p>Usuário Logado</p> : <p>Usuário Deslogado</p>}
+    </HookUseContextProvider>
   );
 }
 
---
+++ react-hooks/src/components/HookUseContext.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { useContext, createContext } from "react";
+
+const HookUseContext = createContext();
+
+export const HookUseContextProvider = ({ children }) => {
+
+
+  return (
+    <HookUseContext.Provider >{children}</HookUseContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useContextHook = () => {
+  return useContext(HookUseContext);
+};
